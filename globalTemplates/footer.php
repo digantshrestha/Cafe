e6de0681f242daa98338bfc790b1fd62a88be4b5
digantshrestha <digantshrestha@gmail.com>
2020-12-08 13:22:54 +0545

    slide test

--- globalTemplates/footer.php
+++ globalTemplates/footer.php
@@ -1,9 +1,6 @@
 <footer>
     <p><?php echo $brandName['brandname']; ?> &copy; 2020</p>
 </footer>
-  <script src="glider.js"></script>
-  <script src="main.js"></script>
-
   <!-- <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVzTaQzz2AA1l4TB_LGoe1O7RP3OdgY34&callback=initMap"></script> -->
         
@@ -16,6 +13,10 @@
     crossorigin="anonymous"></script>
 
   <script src="bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="glider.js"></script>
+  <script src="main.js"></script>
+
 </body>
 
 </html>
