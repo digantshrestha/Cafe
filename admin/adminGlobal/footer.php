347e92105e69168d9325ec391d15086d98efaa4c
digantshrestha <digantshrestha@gmail.com>
2020-12-08 16:41:05 +0545

    js update

--- admin/adminGlobal/footer.php
+++ admin/adminGlobal/footer.php
@@ -6,13 +6,10 @@
   <script src="glider.js"></script>
 
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="bootstrap/js/jquery.slim.min.js"></script>
+  <script src="bootstrap/js/popper.min.js"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
 
   <script src="bootstrap/js/bootstrap.min.js"></script>
   <script src="main.js"></script>
--- globalTemplates/footer.php
+++ globalTemplates/footer.php
@@ -3,19 +3,53 @@
 </footer>
   <!-- <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVzTaQzz2AA1l4TB_LGoe1O7RP3OdgY34&callback=initMap"></script> -->
-        
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
+  <script src="/glider.js"></script>
+
+  <script src="bootstrap/js/jquery.slim.min.js"></script>
+  <script src="bootstrap/js/popper.min.js"></script>
 
   <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script>
+window.addEventListener('load', function () {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.dots',
+        rewind: true,
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [
+            {
+                breakpoint: 765,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 315,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});    
 
-  <script src="glider.js"></script>
-  <script src="main.js"></script>
+  </script>
+  <script src="/main.js"></script>
 
 </body>
 
